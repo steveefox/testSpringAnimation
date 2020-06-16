af937d4d0479a1b5269a7b80bdcf6c24de6d5f58
Nikita Begletskiy <steveefox50@icloud.com>
2020-06-16 14:15:00 +0300

    Initial Commit

---
+++ testSpringAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testSpringAnimation
+//
+//  Created by Nikita on 6/16/20.
+//  Copyright © 2020 Nikita Begletskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
